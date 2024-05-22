ec5f05945b9de0d609ab2e1abe605e77aeb0c69e
prog-ops <akunbaru14102019@gmail.com>
2024-05-22 12:35:48 +0700

    Station detail screen style

--- app/components/[id]/page.tsx
+++ app/components/[id]/page.tsx
@@ -96,6 +96,46 @@ export default function StationDetail({ params }: Props){
             <h1>{station.address}</h1>
             <h1>{station.city}</h1>
             <h1>{station.state}</h1>
+
+            <p>Operating Hours: {station.operatingHours.alwaysOpen ? 'Always Open' : 'Specific Hours'}</p>
+            {station.operatingHours.openingDays && (
+                <ul>
+                    {station.operatingHours.openingDays.map((day: any, index: number) => (
+                        <li key={index}>{day.day}: {day.startTime} - {day.endTime}</li>
+                    ))}
+                </ul>
+            )}
+
+            <p className='mb-8'>Available Charge Points: {station.total.aggregate.count}</p>
+
+            {station.chargePoints.map((chargePoint: any) => (
+                <div key={chargePoint.id} className="mb-8">
+                    <h2 className="text-xl font-bold mb-4">Charge Point ID: {chargePoint.id}</h2>
+                    <table className="min-w-full max-h-screen bg-white border border-gray-200">
+                        <thead>
+                        <tr>
+                            <th className="px-4 py-2 border border-gray-200">Connector ID</th>
+                            <th className="px-4 py-2 border border-gray-200">Connector Type</th>
+                            <th className="px-4 py-2 border border-gray-200">Max Power (kW)</th>
+                            <th className="px-4 py-2 border border-gray-200">Available</th>
+                            <th className="px-4 py-2 border border-gray-200">Tariff Price per kWh</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {chargePoint.chargePointConnectors.map((connector: any) => (
+                            <tr key={connector.id}>
+                                <td className="px-4 py-2 border border-gray-200">{connector.id}</td>
+                                <td className="px-4 py-2 border border-gray-200">{connector.enumConnector.type}</td>
+                                <td className="px-4 py-2 border border-gray-200">{connector.maxPower} kW</td>
+                                <td className="px-4 py-2 border border-gray-200">{connector.available ? "Yes" : "No"}</td>
+                                <td className="px-4 py-2 border border-gray-200">{connector.tariff.priceKwh} {connector.tariff.currencyId}</td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                </div>
+            ))}
+
         </div>
     )
 }
