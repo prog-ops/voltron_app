440a2578f6a777f987eaff23366e49d765cd592e
prog-ops <akunbaru14102019@gmail.com>
2024-05-22 09:39:15 +0700

    Station detail screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
-'use client'
-import client from "@/app/api/client";
-import {ApolloProvider} from "@apollo/client";
-import Main from "@/app/components/Main";
+import Main from "@/app/components/page";
 
 export default function Home() {
     return (
-        <ApolloProvider client={client}>
-            <main className="flex min-h-screen flex-col items-center justify-between p-24">
-                <Main/>
-            </main>
-        </ApolloProvider>
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <Main/>
+        </main>
     );
 }
