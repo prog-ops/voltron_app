0d2c34ce703b6fe9e99768a55900a7fc333010cc
prog-ops <akunbaru14102019@gmail.com>
2024-05-22 19:23:17 +0700

    Polymorphic main title

---
+++ app/components/MainTitle.tsx
@@ -0,0 +1,13 @@
+import {Heading} from "@/app/custom-components/Text";
+
+const MainTitle = () => {
+    return (
+        <div className="mx-auto px-4 md:text-red-200 lg:text-customGreen mb-4">
+            <Heading as='h1' className="text-2xl md:text-4xl lg:text-6xl">
+                Charge Stations
+            </Heading>
+        </div>
+    );
+};
+
+export default MainTitle;
