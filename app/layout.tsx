3fdae5c17d0fa396153cd8c7cf4a380dcbc02b6f
prog-ops <akunbaru14102019@gmail.com>
2024-05-23 23:56:09 +0700

    Add dark mode switcher

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import React from "react";
 import client from "@/app/api/client";
 import {ApolloProvider} from "@apollo/client";
+import {DarkModeProvider} from "@/app/hooks/DarkModeContext";
 
 const inter = Inter({subsets: ["latin"]});
 
@@ -13,11 +14,13 @@ export default function RootLayout({children}: Readonly<{
 }>) {
     return (
         <ApolloProvider client={client}>
-            <html lang="en">
-            <body className={inter.className}>
-            {children}
-            </body>
-            </html>
+            <DarkModeProvider>
+                <html lang="en">
+                <body className={inter.className}>
+                {children}
+                </body>
+                </html>
+            </DarkModeProvider>
         </ApolloProvider>
     );
 }
